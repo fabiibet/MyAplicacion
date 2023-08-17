905dc9948c55b07f88d780ece32955bcb6d573fe
fabiibet <fab.betancourt@duocuc.cl>
2023-08-16 22:56:28 -0400

    Actividad 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuario: string = '';
+  nombre: string = '';
+  apellido: string = '';
+  nivelEducacion: string = '';
+  fechaNacimiento: string = '';
 
-  constructor() {}
+  constructor(private route: ActivatedRoute, private alertController: AlertController, private navCtrl: NavController) {
+    this.route.queryParams.subscribe(params => {
+      if (params && 'usuario' in params) {
+        this.usuario = params['usuario'];
+      }
+    });
+  }
 
+  limpiarCampos() {
+    this.nombre = '';
+    this.apellido = '';
+    this.nivelEducacion = '';
+    this.fechaNacimiento = '';
+  }
+
+  async mostrarInformacion() {
+    const alert = await this.alertController.create({
+      header: 'Usuario',
+      message: `Su nombre es  ${this.nombre} ${this.apellido}`,
+      buttons: ['Yes'],
+    });
+
+    await alert.present();
+  }
+
+  cerrarSesion() {
+    this.navCtrl.navigateBack('/login'); 
+  }
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  usuario: string = '';
+  contrasena: string = '';
+  esFormularioValido: boolean = false;
+  errorMensaje: string = '';
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    
+    this.restablecerCampos();
   }
 
+  iniciarSesion() {
+    this.errorMensaje = ''; 
+
+    if (this.validarDatos()) {
+      this.navCtrl.navigateForward('/home', {
+        queryParams: { usuario: this.usuario },
+      });
+    } else {
+      this.errorMensaje = 'Por favor, corrija los errores en los campos.';
+    }
+  }
+
+  restablecerCampos() {
+    this.usuario = ''; 
+    this.contrasena = ''; 
+    this.esFormularioValido = false; 
+    this.errorMensaje = ''; 
+  }
+
+  validarDatos(): boolean {
+    const alphanumericRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]*$/;
+    const numericRegex = /^[0-9]{4}$/;
+
+    const esUsuarioValido =
+      this.usuario.length >= 3 && this.usuario.length <= 8 && alphanumericRegex.test(this.usuario);
+    const esContrasenaValida = numericRegex.test(this.contrasena);
+
+    this.esFormularioValido = esUsuarioValido && esContrasenaValida;
+
+    return this.esFormularioValido;
+  }
+
+  validarAlfanumerico() {
+    const alphanumericRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]*$/;
+    if (!alphanumericRegex.test(this.usuario)) {
+      this.errorMensaje = 'El usuario debe ser alfanumérico ';
+    } else {
+      this.errorMensaje = ''; 
+    }
+    this.validarDatos(); 
+  }
+
+  validarContrasenaNumerica() {
+    const numericRegex = /^[0-9]{4}$/;
+    if (!numericRegex.test(this.contrasena)) {
+      this.errorMensaje = 'La contraseña debe ser numérica y contener 4 dígitos.';
+    } else {
+      this.errorMensaje = ''; 
+    }
+    this.validarDatos(); 
+  }
 }
